0a83aa5f9dfe673038dd8db116b9ee26bddb9af0
Shubz <shubzinator@gmail.com>
2014-04-21 21:33:32 +0100

    Amended mail.php

--- mail.php
+++ mail.php
@@ -12,13 +12,4 @@ $text="$text";
 // Enter your email address
 $to ='me@mgshuheb.com';
 $send_contact=mail($to,$name,$email,$text);
-
-// Check, if message sent to your email 
-// display message "We've recived your information"
-if($send_contact){
-echo "Thankyou for your message!";
-}
-else {
-echo "ERROR";
-}
 ?>
