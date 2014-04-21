4a5df516ef4b01b78d465029d409ea46562e6145
Shubz <shubzinator@gmail.com>
2014-04-21 21:22:28 +0100

    Added mail.php

---
+++ mail.php
@@ -0,0 +1,38 @@
+<?php
+if($_POST)
+{
+    $to_Email       = "me@mgshuheb.com"; //Replace with recipient email address
+    $subject        = 'Email from someone crazy!'; //Subject line for emails
+    
+    
+    //check if its an ajax request, exit if not
+    if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
+    
+        //exit script outputting json data
+        $output = json_encode(
+        array(
+            'type'=>'error', 
+            'text' => 'Request must come from Ajax'
+        ));
+        
+        die($output);
+    } 
+    
+    //proceed with PHP email.
+    $headers = 'From: '.$user_Email.'' . "\r\n" .
+    'Reply-To: '.$user_Email.'' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    
+        // send mail
+    $sentMail = @mail($name, $email $text .'  -'.$headers);
+    
+    if(!$sentMail)
+    {
+        $output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
+        die($output);
+    }else{
+        $output = json_encode(array('type'=>'message', 'text' => 'Hi '.$name .' Thank you for your email'));
+        die($output);
+    }
+}
+?>
